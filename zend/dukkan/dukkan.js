8327a5e8a7729614a43f03264a81b9fb6a53128b
deer <362707120@qq.com>
2016-01-16 10:45:10 +0800

    New changelist

---
+++ zend/dukkan/dukkan.js
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+/*
+ * $(function(){ $(".fir_pre").click(function(){
+ * 
+ * $(".pics").animate({ left: "+=470px" },800)
+ * 
+ * }); $(".fir_next").click(function(){ $(".pics").animate({ left: "-=470px"
+ * },800) }); });
+ */
+
+
+window.onload = function(){
+
+};
